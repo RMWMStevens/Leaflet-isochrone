f28a0367a6bb0c00801b6b1d92e40b11ecfa130a
Roel Stevens <38633494+RMWMStevens@users.noreply.github.com>
2023-05-11 14:05:37 +0200

    Initial setup & test of API

---
+++ src/server/IsochroneAPI/IsochroneRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace IsochroneAPI
+{
+    public class IsochroneRequest
+    {
+        public string? IsochroonCode { get; set; }
+
+        public int? KnooppuntId { get; set; }
+
+        public int? Afstand { get; set; }
+    }
+}
--- src/server/IsochroneAPI/Program.cs
+++ src/server/IsochroneAPI/Program.cs
@@ -1,6 +1,14 @@
+using IsochroneAPI;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+app.MapGet("/isochroon", ([FromBody] IsochroneRequest request) =>
+{
+    return $"{request.IsochroonCode}: {request.KnooppuntId}, {request.Afstand} meters";
+});
 
 app.Run();
