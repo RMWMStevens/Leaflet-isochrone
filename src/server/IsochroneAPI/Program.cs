e839c4f46ba449ce933e9a34f1413dc6104aaeb3
Roel Stevens <38633494+RMWMStevens@users.noreply.github.com>
2023-05-11 14:49:10 +0200

    Returned JSON in GeoJSON format

--- src/server/IsochroneAPI/Program.cs
+++ src/server/IsochroneAPI/Program.cs
@@ -1,5 +1,6 @@
 using IsochroneAPI;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -8,7 +9,34 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 app.MapGet("/isochroon", ([FromBody] IsochroneRequest request) =>
 {
-    return $"{request.IsochroonCode}: {request.KnooppuntId}, {request.Afstand} meters";
+    using (var connection = new SqlConnection(connectionString))
+    {
+        using (var command = new SqlCommand("SELECT * FROM [dbo].[KortsteRoutePck_GetGpsVanKnooppunten](@IsochroonCode, @KnooppuntId1, @KortsteRoute)", connection))
+        {
+            command.Parameters.AddWithValue("@IsochroonCode", request.IsochroonCode);
+            command.Parameters.AddWithValue("@KnooppuntId1", request.KnooppuntId);
+            command.Parameters.AddWithValue("@KortsteRoute", request.Afstand);
+
+            connection.Open();
+
+            var geoJson = new GeoJson();
+            geoJson.Features.Add(new());
+            geoJson.Features[0].Geometry.Coordinates.Add(new());
+            var coordinates = geoJson.Features[0].Geometry.Coordinates[0];
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var latitude = reader["Latitude"].ToString();
+                    var longitude = reader["Longitude"].ToString();
+                    coordinates.Add(new() { double.Parse(latitude), double.Parse(longitude) });
+                }
+            }
+
+            return geoJson;
+        }
+    }
 });
 
 app.Run();
