2f18e62c727d98c7d4e14631a5a9437c56dc5c11
Roel Stevens <38633494+RMWMStevens@users.noreply.github.com>
2023-05-18 11:15:27 +0200

    Changed marker text, added static polyline, removed isochrone and Leaflet controls

--- src/client/index.js
+++ src/client/index.js
@@ -25,91 +25,18 @@ fetch('https://localhost:7050/knooppunten')
     knooppunten.forEach((knooppunt, i) => {
       markers[i] = L.marker([knooppunt.latitude, knooppunt.longitude])
         .addTo(map)
-        .bindPopup('<strong>' + knooppunt.knooppuntId + '</strong>');
+        .bindPopup(
+          `<strong>#${knooppunt.knooppuntId}: ${knooppunt.latitude}, ${knooppunt.longitude}</strong>`
+        );
     });
   })
   .catch(error => {
     console.log('Error:', error);
   });
 
-// Call endpoint to retrieve isochrone based on 'knooppunt 4 and with an 'Afstand' of 300 meters
-fetch('https://localhost:7050/isochroon', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    IsochroonCode: 'Standaard',
-    KnooppuntId: '4',
-    Afstand: '300',
-  }),
-})
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data).addTo(map);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
-
-// Create a Leaflet control and add it to the map
-var toggleControl = L.control({ position: 'topright' });
-
-// Define the content of the control
-toggleControl.onAdd = function (map) {
-  // Create a div element for the toggle and add a button
-  var toggleDiv = L.DomUtil.create(
-    'div',
-    'leaflet-bar leaflet-control leaflet-control-custom'
-  );
-  toggleDiv.innerHTML = `
-  <button id="toggle-button" class="btn btn-primary">Sandbox On/Off</button>
-`;
-  // Prevent clicking on the toggle from propagating to the map
-  L.DomEvent.disableClickPropagation(toggleDiv);
-  // Return the div element
-  return toggleDiv;
-};
-
-// Add the control to the map
-toggleControl.addTo(map);
-
-// Add an event listener to the toggle button to toggle a class on the map container
-var toggleButton = document.getElementById('toggle-button');
-toggleButton.addEventListener('click', function () {
-  var mapContainer = document.getElementById('mapid');
-  mapContainer.classList.toggle('toggle');
-});
-
-// Create a Leaflet control and add it to the map
-var menuControl = L.control({ position: 'topright' });
-
-// Define the content of the control
-menuControl.onAdd = function (map) {
-  // Create a div element for the menu and add two range sliders
-  var menuDiv = L.DomUtil.create(
-    'div',
-    'leaflet-bar leaflet-control leaflet-control-custom menu-container'
-  );
-  menuDiv.innerHTML = `
-        <div class="menu-item">
-            <label for="slider1">Factor 1</label>
-            <input type="range" min="0" max="10" value="5" class="slider" id="slider1">
-        </div>
-        <div class="menu-item">
-            <label for="slider2">Factor 2</label>
-            <input type="range" min="0" max="10" value="5" class="slider" id="slider2">
-        </div>
-        <div class="menu-item">
-            <label for="slider2">Factor 3</label>
-            <input type="range" min="0" max="10" value="5" class="slider" id="slider3">
-        </div>
-    `;
-  // Prevent clicking on the menu from propagating to the map
-  L.DomEvent.disableClickPropagation(menuDiv);
-  // Return the div element
-  return menuDiv;
-};
-
-// Add the control to the map
-menuControl.addTo(map);
+var startCoordinates = [51.982197, 5.921658];
+var endCoordinates = [51.982351, 5.92219];
+var polyline = L.polyline([startCoordinates, endCoordinates], {
+  weight: 10,
+  color: 'rgba(30, 144, 255, 0.5)',
+}).addTo(map);
